135bd2bdc39904994ee3363fe7e2868e784d3b21
slixenfeld <slixenfeld@mailbox.org>
2023-11-03 21:03:28 +0100

    Initialize Spring Boot

---
+++ src/test/java/de/hp5c/sms/smsws/SmsWsApplicationTests.java
@@ -0,0 +1,13 @@
+package de.hp5c.sms.smsws;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmsWsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
